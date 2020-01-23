2aa1ff2d86b2955e469ae61bd7880cb96cdb240c
Silwer0k <cupomaaax@gmail.com>
2020-01-23 20:57:04 +0300

    Добавил даблджамп

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     private bool _doubleJump;
     //В какую сторону повернут персонаж: false - влево, true - вправо
     private bool _rightTurning;
+    //Количество оставшихся допольнительных прыжков прыжков (изменяется по ходу выполнения кода)
+    private int _extraJumpsLeft;
 
     public Rigidbody2D rb2d;
     public Animator animator;
@@ -19,8 +21,10 @@ public class PlayerController : MonoBehaviour
     //Коэффициент сглаживания передвижения персонажа, без него как то не живо выглядит
     [Range(0, 0.3f)] public float smoothingKoef = .03f;
     [Range(50f, 400f)] public float jumpForce = 200f;
-    [Range(50f, 400f)] public float doubleJumpForce = 100f;
+    [Range(50f, 400f)] public float extraJumpForce = 100f;
     [Range(0, 3f)] public float shiftMultiplier = 2f;
+    //Задаваемое количество дополнительных прыжков (не учитывая первый). Задаем это число через Editor(не изменяется по ходу выполнения кода)
+    public int extraJumps;
     //Можно ли управлять персонажем в воздухе
     public bool canAirControl;
 
@@ -28,6 +32,11 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayers;
     //Пустой объект, координатами и _groundCheckerRadius определяем стоит ли персонаж на земле или нет
     public Transform groundChecker;
+
+    void Awake()
+    {
+        _extraJumpsLeft = extraJumps;
+    }
 
     // Update is called once per frame
     void Update()
@@ -60,10 +69,11 @@ public class PlayerController : MonoBehaviour
 
         if(_isGrounded || (!_isGrounded && canAirControl))
             MovePlayer(_horizontalMove * Time.fixedDeltaTime);
-        if(_isGrounded)
+        JumpPlayer(_jump);
+        _jump = false;
+        if (_isGrounded)
         {
-            JumpPlayer(_jump);
-            _jump = false;
+            _extraJumpsLeft = extraJumps;
         }
     }
 
@@ -86,10 +96,14 @@ public class PlayerController : MonoBehaviour
 
     void JumpPlayer(bool jump)
     {
-        if(jump)
+        if(jump && (_extraJumpsLeft > 0))
         {
             _isGrounded = false;
+            //если не можем управлять персонажем в воздухе, то гасим только Y-состовляющую velocity, чтобы прыжок был направлен в сторону движения.
+            //если можем - убираем velocity полностью, чтобы при падении дабл джамп работал корректно (не просто гасилось бы падение, а производился прыжок)
+            rb2d.velocity = (canAirControl) ? Vector2.zero : new Vector2(rb2d.velocity.x, 0f);
             rb2d.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
+            _extraJumpsLeft -= 1;
         }
     }
 
