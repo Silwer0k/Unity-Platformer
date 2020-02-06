a04334647263d7d21d959a4e78080b4f026a7a26
Silwer0k <cupomaaax@gmail.com>
2020-02-06 16:59:38 +0300

    Добавил в проект TextMesh Pro (расширенный функционал для текста). Сделал скрипт PlayerCharacteristics.cs, в котором содержится ХП персонажа, методы хила и дамага. А еще сделал отображение ХП через UnityEvents.

---
+++ Assets/Scripts/DamagingObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagingObject : MonoBehaviour
+{
+    [SerializeField] private float _damageValue = 10f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerCharacteristics>() != null)
+        {
+            collision.gameObject.GetComponent<PlayerCharacteristics>().TakeDamage(_damageValue);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCharacteristics.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerCharacteristics : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+
+    private float _currentHealthValue;
+
+    public HealthChangedEvent OnHealthChanged;
+
+    private void Awake()
+    {
+        if (_maxHealth > 0)
+        {
+            _currentHealthValue = _maxHealth;
+            OnHealthChanged.Invoke(_currentHealthValue.ToString());
+        }
+        else
+            Debug.LogError("Inncorrect maximum health value");
+    }
+
+    public void TakeDamage(float damageValue)
+    {
+        if (damageValue > 0)
+        {
+            if (_currentHealthValue - damageValue > 0)
+            {
+                _currentHealthValue -= damageValue;
+                OnHealthChanged.Invoke(_currentHealthValue.ToString());
+            }
+            else
+            {
+                _currentHealthValue = 0;
+                OnHealthChanged.Invoke(_currentHealthValue.ToString());
+                //Destroy player
+            }
+            Debug.Log("current health = " + _currentHealthValue);
+        }
+    }
+
+    public void TakeHeal(float healValue)
+    {
+        if (healValue > 0)
+        {
+            if (healValue + _currentHealthValue <= _maxHealth)
+            {
+                _currentHealthValue += healValue;
+                OnHealthChanged.Invoke(_currentHealthValue.ToString());
+            }
+            else
+            {
+                _currentHealthValue = _maxHealth;
+                OnHealthChanged.Invoke(_currentHealthValue.ToString());
+            } 
+            Debug.Log("current health = " + _currentHealthValue);
+        } 
+    }
+}
+
+[System.Serializable]
+public class HealthChangedEvent : UnityEvent<string> { }
