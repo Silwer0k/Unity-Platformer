77c7e4f6ae9e7bf2d51d4a4c48da2a74fca7fe0e
Silwer0k <cupomaaax@gmail.com>
2020-02-13 17:58:27 +0300

    Собственно сам скрипт (забыл закоммитить эти два файла)

---
+++ Assets/Scripts/LockCameraByGround.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System;
+
+public class LockCameraByGround : MonoBehaviour
+{
+    [SerializeField] private Collider2D _player;
+    [SerializeField] private LayerMask _groundLayers;
+    [SerializeField] private float _verticalScreenPositionOnGround;
+    [SerializeField] private float _defaultScreenPosition;
+
+    private void Update()
+    {
+        if (_player.IsTouchingLayers(_groundLayers))
+        {
+            ChangeVCamScreenPos(_verticalScreenPositionOnGround);
+        }
+        else
+        {
+            ChangeVCamScreenPos(_defaultScreenPosition);
+        }
+    }
+
+    private CinemachineFramingTransposer GetVirtualCameraTransposer()
+    {
+        if(gameObject.GetComponent<CinemachineVirtualCamera>() != null)
+        {
+            return gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>();
+        }
+        else
+        {
+            throw new NullReferenceException("Cant find VCam");
+        }
+    }
+
+    private void ChangeVCamScreenPos(float screenPos)
+    {
+        var virtualCameraTransposer = GetVirtualCameraTransposer();
+        virtualCameraTransposer.m_ScreenY = Mathf.Lerp(virtualCameraTransposer.m_ScreenY, screenPos, 0.08f);
+    }
+}
