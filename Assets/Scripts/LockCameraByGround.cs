18a0a899e484f3b603347c6f15aaf8f27300d2bf
Silwer0k <cupomaaax@gmail.com>
2020-02-14 20:03:27 +0300

    Сделал спавнер игрока. Пока что спавнит только при начале игры. На днях сделаю респавн при смерти. Внес UI отображения ХП в префаб игрока.

--- Assets/Scripts/LockCameraByGround.cs
+++ Assets/Scripts/LockCameraByGround.cs
@@ -6,21 +6,29 @@ using System;
 
 public class LockCameraByGround : MonoBehaviour
 {
-    [SerializeField] private Collider2D _player;
+    
     [SerializeField] private LayerMask _groundLayers;
     [SerializeField] private float _verticalScreenPositionOnGround;
-    [SerializeField] private float _defaultScreenPosition;
-
+    
+    private float _defaultScreenPosition;
     private CinemachineFramingTransposer _vCamTransposer;
+    private Transform _player;
+    public Transform player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
+    private Collider2D _playerCollider;
 
     private void Awake()
     {
         _vCamTransposer = gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>();
+        _defaultScreenPosition = _vCamTransposer.m_ScreenY;
     }
 
     private void Update()
     {
-        if (_player.IsTouchingLayers(_groundLayers))
+        if ((_playerCollider != null) && _playerCollider.IsTouchingLayers(_groundLayers))
         {
             ChangeVCamScreenPos(_verticalScreenPositionOnGround);
         }
@@ -33,5 +41,10 @@ public class LockCameraByGround : MonoBehaviour
     private void ChangeVCamScreenPos(float screenPos)
     {
         _vCamTransposer.m_ScreenY = Mathf.Lerp(_vCamTransposer.m_ScreenY, screenPos, 0.08f);
+    }
+
+    public void GetPlayerCollider()
+    {
+        _playerCollider = _player.GetComponent<Collider2D>();
     }
 }
---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System;
+using UnityEngine.Events;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private Transform _spawnPoint;
+
+    public PlayerSpawnedEvent OnPlayerSpawned;
+
+    private void Start()
+    {
+        if(_playerPrefab != null)
+        {
+            var player = Instantiate(_playerPrefab, _spawnPoint.position, Quaternion.identity);
+            OnPlayerSpawned.Invoke(player.transform);
+        }
+        else
+        {
+            throw new NullReferenceException("Player prefab isnt set");
+        }
+    }
+}
+
+[Serializable]
+public class PlayerSpawnedEvent: UnityEvent<Transform> { }
